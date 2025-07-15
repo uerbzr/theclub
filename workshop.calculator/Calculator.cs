87fb024280ee08764a31d98ce57c1b8692c1bc4f
uerbzr <nigel.sibbert@gmail.com>
2025-07-15 11:08:37 +0100

    setup solution, project etc

---
+++ workshop.calculator/Calculator.cs
@@ -0,0 +1,6 @@
+﻿namespace workshop.calculator;
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+}
---
+++ workshop.tests/CalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using workshop.calculator;
+
+namespace workshop.tests;
+
+public class Tests
+{
+
+    [Test]
+    public void TestTwoAddOneEqualsThree()
+    {
+        Calculator calculator = new Calculator();
+
+        int a = 1;
+        int b = 2;
+
+        int result = calculator.Add(a, b);
+
+        Assert.That(result, Is.EqualTo(3));
+    }
+    [TestCase(2,2,4)]
+    [TestCase(12, 2, 14)]
+    [TestCase(20, 2, 22)]
+    [TestCase(1, 2, 3)]
+    [TestCase(2, 2, 4)]
+    public void TestTwoAddOneEqualsThree(int a, int b, int expectedResult)
+    {
+        Calculator calculator = new Calculator();
+
+
+        int actualResult = calculator.Add(a, b);
+
+        Assert.That(actualResult, Is.EqualTo(expectedResult));
+    }
+}
